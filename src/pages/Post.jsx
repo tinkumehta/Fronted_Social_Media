3ba054439430403b20f924d88e9b76f19b652a27
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-04-15 14:39:00 +0530

    feat: Add PostForm, AllPost, EditPost, Home, Login, Post, and Signup components

---
+++ src/pages/Post.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import {Container, PostCard} from "../components"
+import appwriteService from "../appwrite/config";
+
+function Post() {
+  return (
+    <div>Post</div>
+  )
+}
+
+export default Post
